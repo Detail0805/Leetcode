68b6513ebc0cc7c73b1bec9d0e497f5099644c17
max.shih <meteorboy321@gmail.com>
2018-08-01 19:14:10 +0800

    Bulls and Cows
    2A1B

---
+++ src/main/java/SimpleMaths/leetcode/BullsandCows.java
@@ -0,0 +1,34 @@
+package SimpleMaths.leetcode;
+
+public class BullsandCows {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(BullsandCows.BullsandCows("1532","1234"));
+	}
+
+	/**
+	 * 299. Bulls and Cows
+	 * Input: secret = "1807", guess = "7810"
+	 * Output: "1A3B"
+	 * Explanation: 1 bull and 3 cows. The bull is 8, the cows are 0, 1 and 7.
+	 */
+//1532  1234
+	public static String  BullsandCows(String secret, String guess) {
+	       int bulls = 0;
+	        int cows = 0;
+	        int[] numbers = new int[10];
+	        for (int i = 0; i<secret.length(); i++) {
+	            if (secret.charAt(i) == guess.charAt(i)) 
+	            	bulls++;
+	            else {
+	                if (numbers[secret.charAt(i)-'0']++ < 0) 
+	                	cows++;
+	                if (numbers[guess.charAt(i)-'0']-- > 0) 
+	                	cows++;
+	            }
+	        }
+	        return bulls + "A" + cows + "B";
+	}
+	
+}

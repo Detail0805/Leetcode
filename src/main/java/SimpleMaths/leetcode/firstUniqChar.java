d88670e7374857de6c9244f5871178fa70cc83ae
max.shih <meteorboy321@gmail.com>
2019-06-03 16:02:06 +0800

    resteeeeessss

---
+++ src/main/java/SimpleMaths/leetcode/firstUniqChar.java
@@ -0,0 +1,29 @@
+package SimpleMaths.leetcode;
+
+public class firstUniqChar {
+
+	public static void main(String[] args) {
+
+		firstUniqChar("leetcode");
+	
+		
+	}
+	// Time: O(n), Space: O(m)
+	public static int firstUniqChar(String s){
+        int position=-1;
+        int[] a=new int[26];
+        for(int i=0;i<s.length();i++){
+            a[s.charAt(i)-'a']++;    
+            
+        }
+        
+        for(int i=0;i<s.length();i++){
+            int b=a[s.charAt(i)-'a'];
+            if(b==1){
+                return i;
+            }
+        }
+        return position;
+	}
+
+}
